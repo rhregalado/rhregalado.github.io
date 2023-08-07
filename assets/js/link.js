bf2b2471409e95c7671242497235d2e68ff46835
Ric Ryan Regalado <140937746+rhregalado@users.noreply.github.com>
2023-08-07 23:39:48 +0800

    Create link.js

---
+++ assets/js/link.js
@@ -0,0 +1,10 @@
+var $temp = $("<input>");
+var $url = $(location).attr('href');
+
+$('.clipboard').on('click', function() {
+  $("body").append($temp);
+  $temp.val($url).select();
+  document.execCommand("copy");
+  $temp.remove();
+  $("p").text("Copied!");
+})
